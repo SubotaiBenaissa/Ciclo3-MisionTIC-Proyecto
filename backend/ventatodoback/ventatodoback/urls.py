63711973889ac9b7778b613a93b9b99455e768bd
Alangobe <alangobe01@gmail.com>
2022-09-24 18:19:56 -0500

    Añadido DRF yasg para documentación

--- backend/ventatodoback/ventatodoback/urls.py
+++ backend/ventatodoback/ventatodoback/urls.py
@@ -16,12 +16,25 @@ Including another URLconf
 from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
 from Productos.router import routerProducto
 from Usuarios.router import routerUsuario
 from Categoria.router import routerCategoria
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title='VentaTodo API',
+        default_version='v1',
+        description='API Rest de VentaTodo',        
+    ),
+    public=True
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redocs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api/', include(routerProducto.urls)),
     path('api/', include(routerUsuario.urls)),
     path('api/', include(routerCategoria.urls))
