086c09b461d7f20642dc98fd438451c8c32b769b
Alangobe <alangobe01@gmail.com>
2022-09-29 00:35:56 -0500

    XD

--- frontend/ventatodo/src/ui/components/FooterComponent.jsx
+++ frontend/ventatodo/src/ui/components/FooterComponent.jsx
@@ -6,7 +6,7 @@ export const FooterComponent = () => {
         
         <footer className="mt-auto bg-dark text-center text-lg-start">
             <div className="text-center p-3">
-                <span className="text-light">VentaTodo derechos reservados</span>
+                <span className="text-light">© VentaTodo derechos reservados</span>
             </div>
         </footer>
 
